7418605bc6c4e646c63dbe241b71e9391f493dd1
Agus Suhanto <agus.suhanto@gmail.com>
2016-10-11 11:22:30 +0700

    Project structure created
    This is base structure, please proceed with project tasks.

---
+++ DotMercy/DotMercy/Dynamic/List.aspx.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotMercy.Dynamic
+{
+    public partial class List : DotWeb.UI.ListPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
