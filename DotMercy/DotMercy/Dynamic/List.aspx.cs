e66253522f96f71e95ce91167804f551d3600393
Agus Suhanto <agus.suhanto@gmail.com>
2016-10-12 09:43:21 +0700

    Change .gitignore to include *.PDB files

--- DotMercy/DotMercy/Dynamic/List.aspx.cs
+++ DotMercy/DotMercy/Dynamic/List.aspx.cs
@@ -4,9 +4,5 @@ namespace DotMercy.Dynamic
 {
     public partial class List : DotWeb.UI.ListPage
     {
-        protected void Page_Load(object sender, EventArgs e)
-        {
-
-        }
     }
 }
